6df8aed45d5922d72116e79bf6594f379a25089b
andy_1 <870076@qq.com.cn>
2013-06-03 23:16:05 +0800

    chage_1

--- MyBlog/php/comment.php
+++ MyBlog/php/comment.php
@@ -12,5 +12,10 @@
 	//error_log($_POST["index"].$_POST["user"].$_POST["comment"]);
 	error_log($_POST["index"]."][".$_POST["user"]."][".$_POST["comment"]);
 
+	$arr_1 = array('t_date'=>'You are ...');
+	$json_string = json_encode($arr_1);		//序列化json对象.
+	$response=$json_string;				
+	echo $response;					//返回json对象.
+
 	//error_log($index.$user.$comment);
 ?>
