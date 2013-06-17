52bf986aa74a2e3e6524099aed2a5cfc74a5ad73
anna <87070628@qq.com>
2013-06-17 16:11:00 +0800

    change ing

--- MyBlog/php/1.php
+++
@@ -1,5 +0,0 @@
-<?php
-phpinfo();
-//error_reporting(E_ALL);	//打开所有的错误日志.
-//error_log("111111");		//string
-?>
--- MyBlog/php/message_board.php
+++ MyBlog/php/message_board.php
@@ -1,12 +1,24 @@
 
 <?php
-//获得来自 URL 的 q 参数
+//载入数据库操作函数.
+require './connect_mysql.php';
+
 error_reporting(E_ALL);	//打开所有的错误日志.
 
+//获得来自 URL 的 q 参数
 $u=$_POST["userName"];
 $p=$_POST["psw"];
 
 error_log("xxx".$u."yyy".$p);
+
+//入库操作.
+$temp_t = connect_sql();
+error_log($temp_t);
+
+//$sql_data = query_sql("insert into comment values(NULL,$page_index,'$comment_str',NOW(),$userid);");
+//error_log($sql_data);
+
+close_sql();
 
 $arr_1 = array('t_date'=>'You are ...');
 $json_string = json_encode($arr_1);		//序列化json对象.
