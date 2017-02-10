94ebd91775a0ff5ab91cf210545c7769792d6c8e
huangyq <huangyq@changit.cn>
2017-02-10 13:23:01 +0800

    完善风牌

--- Todo.lua
+++ Todo.lua
@@ -2,23 +2,23 @@
 
 --表的复制
 function copyTab(st)
-    local tab = {}
-    for k, v in pairs(st or {}) do
-        if type(v) ~= "table" then
-            tab[k] = v
-        else
-            tab[k] = copyTab(v)
-        end
-    end
-    return tab
+	local tab = {}
+	for k, v in pairs(st or {}) do
+		if type(v) ~= "table" then
+			tab[k] = v
+		else
+			tab[k] = copyTab(v)
+		end
+	end
+	return tab
 end
 --
 function removebrand(bs, brand, nums)
 	local count = 0
-    if bs ~= nil then
-        for i = #bs, 1, -1 do
-            if bs[i] == brand then
-                table.remove(bs, i)
+	if bs ~= nil then
+		for i = #bs, 1, -1 do
+			if bs[i] == brand then
+				table.remove(bs, i)
 				count = count + 1
 				if(nums == 1)then
 					--只删除一张
@@ -35,10 +35,19 @@ function removebrand(bs, brand, nums)
 					end
 				end
 				--全部删除
-            end
-        end
-    end
+			end
+		end
+	end
 end
+--
+local function hasCard(bs, brand)
+	for i, v in ipairs(bs)do
+		if(v == brand)then
+			return true
+		end
+	end
+end
+
 --
 local function hasDui(bs, brand)
 	local count = 0
@@ -61,20 +70,92 @@ local function hasPeng(bs, brand)
 	return count >= 3
 end
 
+local function removeYCcard(bs)
+	local brand = bs[1];
+	if(math.floor(brand / 10) == 0)then
+		--处理存在中发白任意一张,要组成一簇牌,则必须拥有其他两张
+		if(hasCard(bs, 5) or hasCard(bs, 6) or hasCard(bs, 7))then
+			if((removebrand(bs, 5, 1)) and removebrand(bs, 6, 1) and removebrand(bs, 7, 1))then
+				return true
+			else
+				return false
+			end
+		end
 
+		--处理四风,在6张牌的情况下且没有碰牌的情况下,必然存在两个对子
+		--组合簇牌时,优先拆对子
+		brand = 0
+		local brand_2 = 0
+		local brand_3 = 0
+		for i=1, #bs do
+			if(hasDui(bs, bs[i]))then
+				brand = bs[i]
+				break
+			end
+		end
+		for i=1, #bs do
+			if(hasDui(bs, bs[i]) and bs[i] ~= brand)then
+				brand_1 = bs[i]
+				break
+			end
+		end
+		for i=1, #bs do
+			if(bs[i] ~= brand and bs[i] ~= brand_1)then
+				brand_2 = bs[i]
+				break
+			end
+		end
+
+		if(brand ~= 0 and brand_1 ~= 0 and brand_2 ~= 0)then
+			if((removebrand(bs, brand, 1)) and removebrand(bs, brand_2, 1) and removebrand(bs, brand_3, 1))then
+				return true
+			end
+		end
+	else
+		if(brand <= 7 and (removebrand(bs, brand, 1)) and removebrand(bs, brand + 1, 1) and removebrand(bs, brand + 2, 1))then
+			return true
+		end
+	end
+	return false
+end
 
 
 --处理3张
 local function process3card(bs)
 	--table.foreachi(bs, function(i,v) print(v); end)
-
-
 	local brand = bs[1];
 	if(hasPeng(bs, brand))then
 		return true
 	else
-		if(brand <= 7 and bs[2] == brand + 1 and bs[3] == brand + 2)then
-			return true
+		if(math.floor(brand / 10) == 0)then
+			--字牌处理
+			local feng = false
+			if(brand > 4)then
+				feng = false
+			else
+				feng = true
+			end
+			for i = 1, #bs do
+				if(feng)then
+					--如果第一张牌是风,却出现了中发白
+					if(bs[i] > 4)then
+						return false
+					end
+				else
+					--如果第一张牌是中发白,却出现了风牌
+					if(bs[i] <= 4)then
+						return false
+					end
+				end
+			end
+
+			if(bs[1] ~= bs[2] and bs[1] ~= bs[3] and bs[2] ~= bs[3])then
+				return true
+			end
+		else
+			if(brand <= 7 and bs[2] == brand + 1 and bs[3] == brand + 2)then
+				return true
+			end
 		end
 	end
 	return false
@@ -109,15 +190,11 @@ local function process6card(bs)
 
 	--2连簇
 	cbs = copyTab(bs)
-	local brand = cbs[1]
-	if(removebrand(cbs, brand + 1, 1) and removebrand(cbs, brand + 2, 1))then
-		if(removebrand(cbs, brand, 1))then
-			if(process3card(cbs))then
-				return true;
-			end
+	if(removeYCcard(cbs))then
+		if(process3card(cbs))then
+			return true;
 		end
 	end
-
 	return false;
 end
 
@@ -149,12 +226,9 @@ local function process9card(bs)
 
 	--处理3
 	cbs = copyTab(bs)
-	local brand = cbs[1]
-	if(removebrand(cbs, brand + 1, 1) and removebrand(cbs, brand + 2, 1))then
-		if(removebrand(cbs, brand, 1))then
-			if(process6card(cbs))then
-				return true;
-			end
+	if(removeYCcard(cbs))then
+		if(process6card(cbs))then
+			return true;
 		end
 	end
 	return false
@@ -187,12 +261,9 @@ local function process12card(bs)
 	end
 	--处理3
 	cbs = copyTab(bs)
-	local brand = cbs[1]
-	if(removebrand(cbs, brand + 1, 1) and removebrand(cbs, brand + 2, 1))then
-		if(removebrand(cbs, brand, 1))then
-			if(process9card(cbs))then
-				return true;
-			end
+	if(removeYCcard(cbs))then
+		if(process9card(cbs))then
+			return true;
 		end
 	end
 	return false
@@ -204,7 +275,7 @@ local function processhasDuicard(bs)
 	cbs = copyTab(bs)
 	local front = 0
 	for i, v in ipairs(cbs)do
-			while(true)do
+		while(true)do
 			if(v == front)then
 				break;
 			end
