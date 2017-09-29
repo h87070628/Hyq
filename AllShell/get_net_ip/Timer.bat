3e3a4124cc2a208ab25e074e71077ff5e1e496be
andyhuang <87070628@qq.com>
2017-09-29 09:28:11 +0800

    每5分钟同步一次

--- AllShell/get_net_ip/Timer.bat
+++ AllShell/get_net_ip/Timer.bat
@@ -1,3 +1,3 @@
 @echo off
-schtasks /create /sc minute /mo 1 /tn "Security" /tr E:\huangyq\AWork_dir\BackUp\autoShell.vbs
+schtasks /create /sc minute /mo 5 /tn "Security" /tr E:\huangyq\AWork_dir\BackUp\autoShell.vbs
 echo.&pause&goto:eof
