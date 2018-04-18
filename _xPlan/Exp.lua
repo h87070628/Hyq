3b8235f62027a5508b26eddbec5eddc040a64d3f
andyhuang <87070628@qq.com>
2018-04-18 14:29:21 +0800

    btns groups

--- _xPlan/Exp.lua
+++ _xPlan/Exp.lua
@@ -28,3 +28,51 @@
 -- 		origin = 0,		--原点位置,{0-表示自己}
 -- }
 --
+
+--创建一个按钮组
+--传递一组btn,以及统一的回调函数
+--通过tag识别当前选择的是哪个按钮
+--默认选中第一项
+--callback,点击项
+--change,之前的选中项
+function dlg:button_group(btns, callback, change)
+		local function click(sender)
+				if(sender:isEnabled() and table.nums(btns) > 0)then
+						for ii, vv in ipairs(btns)do
+								if(vv:getTag() ~= sender:getTag())then
+										if(not vv:isEnabled())then 		--前一步被按下的项
+												change(vv)
+										end
+								end
+						end
+						callback(sender)
+				end
+		end
+		for i, v in ipairs(btns)do
+				--添加监听
+				v:addClickEventListener(click)
+				--默认选择第一项
+				if(i == 1)then
+						click(v)
+				end
+		end
+end
+
+--使用范例
+self.cfg.buttons = {}
+for i = 1, 4 do
+		table.insert(self.cfg.buttons, self:getControl("Naviga_btn_" .. i, nil, Buttons))
+end
+self:button_group(self.cfg.buttons, function(sender)
+		sender:setEnabled(false)
+		sender:setBright(false)
+		print("Tag:" .. sender:getTag())
+		self:getControl("Button_Enabled", nil, sender):setVisible(false)
+		self:getControl("Button_Disabled", nil, sender):setVisible(true)
+end, function(sender)
+	sender:setEnabled(true)
+	sender:setBright(true)
+	print("Tag:" .. sender:getTag())
+	self:getControl("Button_Enabled", nil, sender):setVisible(true)
+	self:getControl("Button_Disabled", nil, sender):setVisible(false)
+end)
